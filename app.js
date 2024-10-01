fff8e1775d58f4ca6078429ebd45f13d23a0c592
pslcorrea <pslcorrea@gmail.com>
2024-10-01 15:36:56 -0300

    instalação do helmet para geração de cabeçalhos

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 import express from "express";
 import driversRouter from "./routes/driver.js";
 import teamsRouter from "./routes/team.js";
+import helmet from "helmet";
 
 const baseAPIRoute = "/api/v1";
 
 const app = express();
 
 app.use(express.json());
+//? Gera cabeçalhos de proteção para respostas HTTP
+app.use(helmet());
 app.use(baseAPIRoute + "/drivers", driversRouter);
 app.use(baseAPIRoute + "/teams", teamsRouter);
 
